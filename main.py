04e7d3e7e464e751469db51a65e3da85461a2725
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-09-13 14:49:04 +0900

    データベースの使用

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import sqlite3
 
 Token = "MTA5NTI1MjQ0ODYwMTQ1NjY3Mg.GjaVkI.k8OJ16DqLE1SxwHSoCiXrz20oVF5agg3JtzfOY"
 intents = discord.Intents.default()
@@ -7,6 +8,19 @@ intents.message_content = True
 # intents.guilds = True
 client = discord.Client(intents=intents)
 
+dbName="debt.db"
+
+def useDB(creditor,messageList):
+    [debtor,amount,notes]=messageList
+    conn=sqlite3.connect(dbName)
+    cur=conn.cursor()
+    cur.execute("create table debtPrompt(creditor STRING,debtor STRING,amount INTEGER,notes STRING)")
+    cur.execute("insert into debtPrompt(creditor) values("+creditor+")")
+    cur.execute("insert into debtPrompt(debtor) values("+debtor+")")
+    cur.execute("insert into debtPrompt(amount) values("+amount+")")
+    cur.execute("insert into debtPrompt(notes) values("+notes+")")
+    conn.commit()
+    conn.close()
 
 @client.event
 async def on_ready():
@@ -18,5 +32,7 @@ async def on_message(message):
     if message.author==client.user:
         return 
     messageList=message.channel.send(message.content.split())
+    await useDB(message.autor,messageList)
+    
     
 client.run(Token)
