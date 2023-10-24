e994a655387bf16a192fd0610c497dce64256b59
TanakaKotaro <kotaro.tanaka@4nonome.com>
2023-10-24 15:34:22 +0900

    サーバー内のメンバーを取得

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import re
 Token = "MTA5NTI1MjQ0ODYwMTQ1NjY3Mg.GjaVkI.k8OJ16DqLE1SxwHSoCiXrz20oVF5agg3JtzfOY"
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members=True
+intents.typing=False
 # intents.reactions = True
 # intents.guilds = True
 client = discord.Client(intents=intents)
@@ -51,11 +53,11 @@ def showCredit(creditor):
 def arrangeList(lists: list):
     newLists = []
     for list in lists:
-        l = list[0:1:1]
+        l = list[0:2:1]
         newLists.append(l)
     return newLists
 
-def splitList(lists: list, members: list):
+def splitList(lists: list, members):
     returnObject = {}
     for list in lists:
         counter = 0
@@ -72,18 +74,25 @@ def searchCheck(message: discord.Message):
             checker = True
     return checker
 
-async def showHistory(isDebtor:bool,person,message):
-    guild = client.get_guild(message.guild.id)
-    members=guild._members
+async def showHistory(isDebtor:bool,person,message,member):
     data=0
     if isDebtor:
         data=showDebt(message.mentions[1].name)
     else:
         data= showCredit(message.mentions[1].name)
     arrangeData=arrangeList(data)
-    splitData=splitList(arrangeData,members)
-    for data in splitData:
-        await message.channel.send(data)
+    splitData=splitList(arrangeData,member)
+    await message.channel.send(splitData)
+    # for datum in splitData:
+    #     await message.channel.send(splitData[datum])
+
+async def getMember(message):
+    guild = client.get_guild(message.guild.id)
+    members=guild._members
+    memberList=[]
+    for member in members:
+        memberList.append(member)
+    return memberList
 
 @client.event
 async def on_ready():
@@ -97,7 +106,8 @@ async def on_message(message):
 
     for mention in message.mentions:
         if mention.name == "借金催促":
-            await showHistory(searchCheck(message),message.mentions[1].name,message)
+            member= await getMember(message)
+            await showHistory(searchCheck(message),message.mentions[1].name,message,member)
         else:
             messageList = message.content.split()
 
