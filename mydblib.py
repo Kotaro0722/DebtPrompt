7d2642142d306b8ff6db338a8fcd70037d5dad8d
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-02-15 02:39:57 +0900

    モジュール修正

--- mydblib.py
+++ mydblib.py
@@ -1,26 +1,25 @@
-import MySQLdb
+import mysql.connector as mydb
 import pandas as pd
 import sys
 
 
 def my_select(db, sql_string):
     try:
-        dbcon = MySQLdb.connect(
+        dbcon = mydb.connect(
             host="localhost",
             user="root",
             password="kotaro0722",
             database=db
         )
-        cursor = dbcon.cursor()
-    except MySQLdb.Error as e:
+        cursor = dbcon.cursor(dictionary=True)
+    except mydb.Error as e:
         print(f"DBコネクションでエラー発生\n{e}")
         sys.exit()
 
     try:
         cursor.execute(sql_string)
         recset = cursor.fetchall()
-        print(recset)
-    except MySQLdb.Error as e:
+    except mydb.Error as e:
         print(f"クエリ実行でエラー発生\n{e}")
         print(f"入力されたSQLは\n{sql_string}")
         sys.exit()
