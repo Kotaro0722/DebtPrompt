b142bb56911ccc069a1ec9263d5778b902462b05
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-02-20 19:07:03 +0900

    db.pyの修正

--- db.py
+++ db.py
@@ -4,11 +4,11 @@ import config
 # print(config.HOST)
 
 conn = mydb.connect(
-    host="viaduct.proxy.rlwy.net",
-    user="root",
-    password="5HAH5Db63Chf1eAGB5fBf-A2CH42Aecg",
-    port=10029,
-    db="railway"
+    host=config.HOST,
+    user=config.USER,
+    password=config.PASSWORD,
+    port=config.PORT,
+    db=config.DBNAME
 )
 
 cursor = conn.cursor()
@@ -21,9 +21,10 @@ sql_alter_column1 = "ALTER TABLE debt MODIFY COLUMN id BIGINT"
 sql_alter_column2 = "ALTER TABLE debt MODIFY COLUMN creditor BIGINT, MODIFY COLUMN debtor BIGINT"
 sql_delete_data = "DELETE FROM debt WHERE id = 1;"
 sql_select_data = "SELECT debtor,amount,ispay FROM debt WHERE ispay=0 AND creditor=960825958208765973"
-sql_delete_table = "DROP TABLE test1"
+sql_delete_table = "DROP TABLE debt"
+sql_delete_table_2 = "DROP TABLE sum_1209423440495509504"
 
-cursor.execute(sql_show_table)
+cursor.execute(sql_create_table)
 
 for row in cursor:
     print(row)
