9617e230d40ac98135f802f359eca7150e205c45
TanakaKotaro <kotaro.tanaka@4nonome.com>
2024-04-24 14:10:41 +0900

    ファイル分割

--- debtManager.py
+++ debtManager.py
@@ -141,3 +141,96 @@ async def showDetail(message_id: discord.Message, channel):
     data = my_select(dbName, sql_string)
     for i in range(len(data)):
         await channel.send(f"[その{i+1}](<https://discord.com/channels/963060474646257675/1098819625346682981/{data.at[i,'id']}>)")
+
+
+
+async def debt_manager_on_message(message:discord.Message,client:discord.Client):
+    if message.author == client.user:
+        return
+
+    message_content = message.content
+    pattern_is_summon = f"<@{client.user.id}>"
+    is_summon = re.match(pattern_is_summon, message_content)
+    if is_summon:
+        pattern_is_debtor = pattern_is_summon+r"\s*"+await getDebtor(message)
+        is_debtor = re.fullmatch(pattern_is_debtor, message_content)
+
+        is_all_debt = re.fullmatch(pattern_is_summon, message_content)
+
+        pattern_is_scroll = f"<@{client.user.id}>"+r"\s*"+"scroll"
+        is_scroll = re.fullmatch(pattern_is_scroll, message_content)
+
+        if is_all_debt:
+            await showAllCredit(message.author.id, message)
+
+        elif is_debtor:
+            debtor = re.findall(r"[0-9]+", message_content)[1]
+            await showOneCredit(message.author.id, debtor, message)
+
+        elif is_scroll:
+            register_channel = client.get_channel(int(register_channel_id))
+            await scrollMessage(register_channel)
+
+        else:
+            await message.channel.send("不正な入力です")
+
+    pattern_is_register = await getPatternIsRegister(message)
+    is_register = re.fullmatch(pattern_is_register, message_content)
+    if is_register:
+        pattern_debtor_id = "[0-9]+"
+        debtor = re.findall(pattern_debtor_id, message_content)[0]
+
+        creditor = message.author.id
+
+        pattern_amount = pattern_debtor_id
+        amount = re.findall(pattern_amount, message_content)[1]
+
+        id = message.id
+
+        registerToDB(id, creditor, debtor, amount, 0)
+        await message.add_reaction("⭕")
+
+async def debt_manager_on_raw_reaction_add(payload:discord.RawReactionActionEvent,client:discord.Client):
+    txt_channel = client.get_channel(payload.channel_id)
+    message = await txt_channel.fetch_message(payload.message_id)
+    user = payload.user_id
+
+    if user == client.user:
+        return
+
+    if message.author.id != payload.user_id and message.author.id != client.user.id:
+        return
+
+    if payload.emoji.name == "✅":
+        if list(filter(lambda rea: rea.emoji == "✅", message.reactions))[0].me:
+            await message.remove_reaction("✅", client.user)
+            return
+
+        register_channel = client.get_channel(int(register_channel_id))
+        if message.author.id == client.user.id:
+            await payAllDebt(message.id, register_channel)
+        else:
+            payOneDebt(message.id)
+
+    if payload.emoji.name == "❔" and client.user.id == message.author.id:
+        await showDetail(payload.message_id, txt_channel)
+
+async def debt_manager_on_raw_reaction_remove(payload:discord.RawReactionActionEvent,client:discord.Client):
+    txt_channel = client.get_channel(payload.channel_id)
+    message = await txt_channel.fetch_message(payload.message_id)
+    user = payload.member
+
+    if user == client.user:
+        return
+
+    if message.author.id != payload.user_id and message.author.id != client.user.id:
+        return
+
+    if payload.emoji.name != "✅":
+        return
+
+    register_channel = client.get_channel(int(register_channel_id))
+    if message.author.id == client.user.id:
+        await cancelAllPayDebt(message.id, register_channel)
+    else:
+        cancelOnePayDebt(message.id)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import numpy as np
 import re
 import config
-from debtManager import getDebtor,showAllCredit,showOneCredit,register_channel_id,scrollMessage,getPatternIsRegister,registerToDB,payAllDebt,payOneDebt,showDetail,cancelAllPayDebt,cancelOnePayDebt
+from debtManager import debt_manager_on_message ,debt_manager_on_raw_reaction_add,debt_manager_on_raw_reaction_remove
 
 Token = config.TOKEN
 
@@ -23,98 +23,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
-    if message.author == client.user:
-        return
-
-    message_content = message.content
-    pattern_is_summon = f"<@{client.user.id}>"
-    is_summon = re.match(pattern_is_summon, message_content)
-    if is_summon:
-        pattern_is_debtor = pattern_is_summon+r"\s*"+await getDebtor(message)
-        is_debtor = re.fullmatch(pattern_is_debtor, message_content)
-
-        is_all_debt = re.fullmatch(pattern_is_summon, message_content)
-
-        pattern_is_scroll = f"<@{client.user.id}>"+r"\s*"+"scroll"
-        is_scroll = re.fullmatch(pattern_is_scroll, message_content)
-
-        if is_all_debt:
-            await showAllCredit(message.author.id, message)
-
-        elif is_debtor:
-            debtor = re.findall(r"[0-9]+", message_content)[1]
-            await showOneCredit(message.author.id, debtor, message)
-
-        elif is_scroll:
-            register_channel = client.get_channel(int(register_channel_id))
-            await scrollMessage(register_channel)
-
-        else:
-            await message.channel.send("不正な入力です")
-
-    pattern_is_register = await getPatternIsRegister(message)
-    is_register = re.fullmatch(pattern_is_register, message_content)
-    if is_register:
-        pattern_debtor_id = "[0-9]+"
-        debtor = re.findall(pattern_debtor_id, message_content)[0]
-
-        creditor = message.author.id
-
-        pattern_amount = pattern_debtor_id
-        amount = re.findall(pattern_amount, message_content)[1]
-
-        id = message.id
-
-        registerToDB(id, creditor, debtor, amount, 0)
-        await message.add_reaction("⭕")
-
+    await debt_manager_on_message(message,client)
 
 @client.event
 async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
-    txt_channel = client.get_channel(payload.channel_id)
-    message = await txt_channel.fetch_message(payload.message_id)
-    user = payload.user_id
-
-    if user == client.user:
-        return
-
-    if message.author.id != payload.user_id and message.author.id != client.user.id:
-        return
-
-    if payload.emoji.name == "✅":
-        if list(filter(lambda rea: rea.emoji == "✅", message.reactions))[0].me:
-            await message.remove_reaction("✅", client.user)
-            return
-
-        register_channel = client.get_channel(int(register_channel_id))
-        if message.author.id == client.user.id:
-            await payAllDebt(message.id, register_channel)
-        else:
-            payOneDebt(message.id)
-
-    if payload.emoji.name == "❔" and client.user.id == message.author.id:
-        await showDetail(payload.message_id, txt_channel)
-
+    await debt_manager_on_raw_reaction_add(payload,client)
 
 @client.event
 async def on_raw_reaction_remove(payload):
-    txt_channel = client.get_channel(payload.channel_id)
-    message = await txt_channel.fetch_message(payload.message_id)
-    user = payload.member
-
-    if user == client.user:
-        return
-
-    if message.author.id != payload.user_id and message.author.id != client.user.id:
-        return
-
-    if payload.emoji.name != "✅":
-        return
-
-    register_channel = client.get_channel(int(register_channel_id))
-    if message.author.id == client.user.id:
-        await cancelAllPayDebt(message.id, register_channel)
-    else:
-        cancelOnePayDebt(message.id)
+    await debt_manager_on_raw_reaction_remove(payload,client)
 
 client.run(Token)
