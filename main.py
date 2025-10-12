8a7040484d98ea4c76bf57fbb01d1f985bfb6529
Kotaro0722 <kotaro.tanaka@4nonome.com>
2025-10-12 18:48:13 +0900

    fix : パス名を変更

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import discord
 import mysql.connector as mydb
 import numpy as np
 import re
-from db.my_select import my_select
-from db.my_update import my_update
+from db.db_select import my_select
+from db.db_select import my_update
 import config
 
 Token = config.TOKEN
